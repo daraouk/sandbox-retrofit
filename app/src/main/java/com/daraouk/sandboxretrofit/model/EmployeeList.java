206698f355587ee48be955228228331f8e1f4170
Dara Ouk <vdb.dara.ouk@gmail.com>
2018-01-07 16:39:09 +0700

    initial commit

---
+++ app/src/main/java/com/daraouk/sandboxretrofit/model/EmployeeList.java
@@ -0,0 +1,18 @@
+package com.daraouk.sandboxretrofit.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+public class EmployeeList {
+    @SerializedName("employeeList")
+    private ArrayList<Employee> employeeList;
+
+    public ArrayList<Employee> getEmployeeArrayList() {
+        return employeeList;
+    }
+
+    public void setEmployeeArrayList(ArrayList<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+}
